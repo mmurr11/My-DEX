dfa249f06d44d760bccf9b4e6825c64106c95893
Matthew Murray <mattm2411@outlook.com>
2021-10-25 00:44:51 -0500

    added new metrics

--- fetch.js
+++ fetch.js
@@ -1,5 +1,6 @@
-const list = document.getElementById('metric_list')
+const list = document.getElementById('metric_list') // where metrics will be appended
 
+// flow in/out of exchanges ex. coinbase, binance, etc. for btc/eth 
 async function btcInFlow() {
     try {
         const data = await fetch(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=btc&metrics=FlowInExNtv`).then(response => {
@@ -56,6 +57,7 @@ async function ethOutFlow() {
     }
 }
 
+// subtract outflow from inflow then append result to metric list
 async function btcNetFlow() {
     try {
         btcInFlow().then(x => {
@@ -98,5 +100,109 @@ async function ethNetFlow() {
     }
 }
 
+// Mining hash rates for btc and eth
+async function btcHashRateToday() {
+    try {
+        const data = await fetch(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=btc&metrics=HashRate`).then(response => {
+            return response.json();
+        }).then(btc => {
+            return btc.data[99].HashRate
+        })
+        return data
+    }
+    catch (err) {
+        console.log('error')
+    }
+}
+
+async function btcHashRateLastMonth() {
+    try {
+        const data = await fetch(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=btc&metrics=HashRate`).then(response => {
+            return response.json();
+        }).then(btc => {
+            console.log(btc)
+            return btc.data[69].HashRate
+        })
+        return data
+    }
+    catch (err) {
+        console.log('error')
+    }
+}
+
+// subtract hash rate of 30 days ago from todays then append to metric list
+async function btcHashRateMonthOverMonth() {
+    try {
+        btcHashRateToday().then(x => {
+            return btcHashRateLastMonth().then(y => {
+                return x - y
+            }).then(data => {
+                let div = document.createElement('li')
+                div.className = "metric_row";
+                let html = `
+                <span class="token_list_text">BTC hash rate MoM: ${data}</span>
+                `
+                div.innerHTML = html;
+                list.appendChild(div)
+            })
+        })
+    }
+    catch (err) {
+        console.log('nope')
+    }
+}
+
+async function ethHashRateToday() {
+    try {
+        const data = await fetch(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=eth&metrics=HashRate`).then(response => {
+            return response.json();
+        }).then(eth => {
+            return eth.data[99].HashRate
+        })
+        return data
+    }
+    catch (err) {
+        console.log('error')
+    }
+}
+
+async function ethHashRateLastMonth() {
+    try {
+        const data = await fetch(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=eth&metrics=HashRate`).then(response => {
+            return response.json();
+        }).then(eth => {
+            console.log(eth)
+            return eth.data[69].HashRate
+        })
+        return data
+    }
+    catch (err) {
+        console.log('error')
+    }
+}
+
+async function ethHashRateMonthOverMonth() {
+    try {
+        ethHashRateToday().then(x => {
+            return ethHashRateLastMonth().then(y => {
+                return x - y
+            }).then(data => {
+                let div = document.createElement('li')
+                div.className = "metric_row";
+                let html = `
+                <span class="token_list_text">ETH hash rate MoM: ${data}</span>
+                `
+                div.innerHTML = html;
+                list.appendChild(div)
+            })
+        })
+    }
+    catch (err) {
+        console.log('nope')
+    }
+}
+
 btcNetFlow()
 ethNetFlow()
+btcHashRateMonthOverMonth()
+ethHashRateMonthOverMonth()
